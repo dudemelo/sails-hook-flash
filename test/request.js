dbca5119dfddd89928945a1342592b8ae6fddb8c
dudemelo <dudemelo@gmail.com>
2015-12-14 13:14:18 -0300

    Update the request unit tests

--- test/request.js
+++ test/request.js
@@ -12,32 +12,56 @@ describe('Request tests ::', function () {
     return flash(request, response, done);
   });
 
-  describe("addFlash(type, message)", function () {
+  describe('addFlash(type, message)', function () {
     it('is callable ', function () {
-      assert.func(request.addFlash);
-      assert.ok(request.addFlash(
-        'success',
-        'Is callable and and stores a message.'
-      ))
+      assert.func(
+        request.addFlash,
+        'addFlash must be a function'
+      );
+      assert.ok(request.addFlash('success', 'Stores a message.'));
+      assert.ok(request.addFlash('success', 'Stores another message.'));
     });
 
     it('stores a message', function () {
       var session = request.session[Object.keys(request.session)[0]];
-      assert.object(session, 'Is an object');
-      assert.array(session.success);
+      assert.object(
+        session,
+        'The session must be an object'
+      );
+      assert.array(
+        session.success,
+        'The flash message type must have an array of messages'
+      );
       assert.strictEqual(
         session.success[0],
-        'Is callable and and stores a message.'
+        'Stores a message.'
       );
     });
 
     it('stores more than one message per type', function () {
       var session = request.session[Object.keys(request.session)[0]];
-      assert.object(session, 'Is an object');
-      assert.array(session.success);
       assert.strictEqual(
-        session.success[0],
-        'Is callable and and stores a message.'
+        session.success.length,
+        2,
+        'The flash message type must have an array of messages'
+      );
+    });
+  });
+
+  describe('getFlash(type)', function () {
+    it('is callable', function () {
+      assert.func(
+        request.getFlash,
+        'getFlash must be a function'
+      );
+    });
+  });
+
+  describe('hasFlash(type)', function () {
+    it('is callable', function () {
+      assert.func(
+        request.hasFlash,
+        'hasFlash must be a function'
       );
     });
   });
